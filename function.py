fc8d5dc8d322904c9ff9f0182913c0a4498737f1
anujskya <anujskya18@gmail.com>
2021-07-09 16:30:55 +0545

    monthly

--- function.py
+++ function.py
@@ -1,5 +1,6 @@
 def ask():
-    a = input("enter your salary")
-    print(a)
-    print("ask")
+    salary = int(input("enter your monthly salary"))
+    yearly=salary*12
+    print("Your Yearly Salary =", yearly) 
+    print(yearly)
 ask()
